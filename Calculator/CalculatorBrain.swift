c67f6e3730de6f77c83fc4ded32042e2ca5a93dc
Roberts, Benjamin <benjamin.roberts@sky.uk>
2015-12-07 13:39:49 +0000

    something from ages ago

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -52,6 +52,25 @@ class CalculatorBrain {
         learnOp(Op.UnaryOperation("√", sqrt))
     }
     
+    var program: AnyObject { // guaranteed to be a PorpertyList
+        get {
+            return opStack.map { $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
+    
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
         
         if !ops.isEmpty {
